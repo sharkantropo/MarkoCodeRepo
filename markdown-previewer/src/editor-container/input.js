754f8f66c00a7f0a522197af2d090d7ffab9156d
Sharkantropo <michinaux.marko@gmail.com>
2018-12-12 07:51:18 -0400

    added some files

---
+++ markdown-previewer/src/editor-container/input.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import  from 'react-redux';
+
+class InputTextArea extends Component
+{
+	constructor(props) 
+	{
+		super(props);
+		this.handleChange=this.handleChange.bind(this);
+	}
+	
+	handleChange(e)
+	{
+		// will dispatch event.target.value to store
+	}
+	
+	render() {
+		let placehold="placeholder";
+		return(<textarea id="editor" name="editor" value={placehold} onChange={this.handleChange} />);
+	}
+} 
+
+export default InputTextArea;
---
+++ markdown-previewer/src/previewer-container/output.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import  from 'react-redux';
+
+const OutputTextArea= function (props)
+{
+
+		return(
+            <div id="pick">
+                <textarea id="preview" name="previewer" value={"la mulana"} />
+            </div>
+        );
+} 
+
+export default OutputTextArea;
